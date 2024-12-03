a73e456094171d0df7c29f54af97633f6c9ff120
Mark Bryan Labinay <smokefacebook02@gmail.com>
2024-12-03 22:04:06 +0800

    modify the ui

--- resources/views/Client/dashboard.blade.php
+++ resources/views/Client/dashboard.blade.php
@@ -1,9 +1,9 @@
-<x-client-layout-layout>
+<x-client-layout>
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header">{{ __('Dashboard') }}</div>
+                    <div class="card-header">{{ __('User Dashboard') }}</div>
 
                     <div class="card-body">
                         @if (session('status'))
@@ -18,4 +18,4 @@
             </div>
         </div>
     </div>
-</x-client-layout-layout>
+</x-client-layout>
