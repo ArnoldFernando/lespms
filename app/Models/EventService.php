535eb3b416df42030c4d94330a08b2011d91970a
ianlabicani <labicaniian@gmail.com>
2024-12-04 14:43:40 +0800

    Add fillable properties to EventService model for mass assignment

--- app/Models/EventService.php
+++ app/Models/EventService.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class EventService extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+            'service_provider_id',
+            'title',
+            'description',
+            'rate',
+            'status',
+            'scheduled_date',
+            'available_until',
+            'assigned_to',
+            'location',
+            'special_requests',
+            'is_featured',
+    ];
 }
