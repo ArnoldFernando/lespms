d452827e71ca1d85fba90b84c241c44197279d78
ianlabicani <labicaniian@gmail.com>
2024-12-13 01:59:56 +0800

    implement middlewares in the routes file for better organization

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\DashboardController as AdminDashboardController;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\Resources\UserController ;
 use App\Http\Controllers\Resources\ServiceController;
 use App\Http\Controllers\Resources\EventServiceController;
@@ -31,39 +32,50 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'auth'])->name('home');
+Route::get('/home', [HomeController::class, 'redirectUserBasedOnRole'])
+    ->middleware('auth')
+    ->name('home');
 
 Route::prefix('admin')
     ->middleware(['auth', 'admin'])
     ->group(function () {
-        Route::get('dashboard', [AdminDashboardController::class, 'getData'])->name('admin.dashboard');
-        Route::put('users/{id}/usertype', [UserController::class, 'updateUsertype'])->name('users.updateUsertype');
+        Route::get('dashboard', [AdminDashboardController::class, 'getData'])
+            ->name('admin.dashboard');
+        Route::put('users/{id}/usertype', [UserController::class, 'updateUsertype'])
+            ->name('users.updateUsertype');
         Route::resource('users', UserController::class);
     });
 
 Route::prefix('service-provider')
     ->middleware(['auth', 'service.provider'])
     ->group(function () {
-        Route::get('dashboard', [ServiceProviderDashboardController::class, 'index'])->name('service-provider.dashboard');
+        Route::get('dashboard', [ServiceProviderDashboardController::class, 'index'])
+            ->name('service-provider.dashboard');
         Route::resource('services', EventServiceController::class);
-        Route::get('notifications/recent', [NotificationController::class, 'getRecentNotifications'])->name('notifications.getNotifications');
+        Route::get('notifications/recent', [NotificationController::class, 'getRecentNotifications'])
+            ->name('notifications.getNotifications');
         Route::resource('notifications', NotificationController::class);
-        Route::get('/my-services/bookings', [EventServiceController::class, 'showBookings'])->name('event-services.bookings');
-        Route::post('/my-services/bookings/{bookingId}/{status}', [EventServiceController::class, 'updateStatus'])->name('event-services.updateStatus');
-        Route::get('/booked-users', [BlockuserController::class, 'index'])->name('booked.users');
-        Route::post('/block-user/{user}/service', [BlockuserController::class, 'blockUserFromServices'])->name('user.block.service');
-        Route::patch('/user/unblock/{userId}', [BlockuserController::class, 'unblockUser'])->name('service.user.unblock');
+        Route::get('/my-services/bookings', [EventServiceController::class, 'showBookings'])
+            ->name('event-services.bookings');
+        Route::post('/my-services/bookings/{bookingId}/{status}', [EventServiceController::class, 'updateStatus'])
+            ->name('event-services.updateStatus');
+        Route::get('/booked-users', [BlockuserController::class, 'index'])
+            ->name('booked.users');
+        Route::post('/block-user/{user}/service', [BlockuserController::class, 'blockUserFromServices'])
+            ->name('user.block.service');
+        Route::patch('/user/unblock/{userId}', [BlockuserController::class, 'unblockUser'])
+            ->name('service.user.unblock');
     });
 
 
-    // TODO: refactor the routes below
-
+// TODO: refactor the routes below
 // user routes
 Route::middleware(['auth', 'user'])->group(function () {
     route::view('user', 'Client.dashboard');
     Route::prefix('client')->name('client.')->group(function () {
         route::resource('service', ServiceController::class);
-        route::get('servicedetails/{id}', [ServiceController::class, 'view_details'])->name('view-details');
+        route::get('servicedetails/{id}', [ServiceController::class, 'view_details'])
+            ->name('view-details');
         route::resource('bookings', BookingController::class);
     });
 });
