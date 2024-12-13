ea832bb3279ef2c068e6479cce9c81301d615f08
ianlabicani <labicaniian@gmail.com>
2024-12-13 22:45:45 +0800

    enhance user index view with responsive design and improved data presentation

--- resources/views/Admin/users/index.blade.php
+++ resources/views/Admin/users/index.blade.php
@@ -1,33 +1,37 @@
 <x-app-layout>
-    <table>
-        <thead>
-            <tr>
-                <th>Name</th>
-                <th>Email</th>
-                <th>User Type</th>
-                <th>is_blocked</th>
-                <th>blocked_by</th>
-                <th>created_at</th>
-                <th>updated_at</th>
-                <th>Actions</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($users as $user)
-                <tr>
-                    <td>{{ $user->name }}</td>
-                    <td>{{ $user->email }}</td>
-                    <td>{{ $user->usertype }}</td>
-                    <td>{{ $user->is_blocked }}</td>
-                    <td>{{ $user->blocked_by }}</td>
-                    <td>{{ $user->created_at }}</td>
-                    <td>{{ $user->updated_at }}</td>
-                    <td>
-                        <a href="{{ route('users.show', $user->id) }}">View</a>
-                    </td>
-                </tr>
-            @endforeach
-
-        </tbody>
-    </table>
+    <div class="container my-5">
+        <div class="table-responsive">
+            <table class="table table-striped table-bordered">
+                <thead class="thead-dark">
+                    <tr>
+                        <th scope="col">Name</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">User Type</th>
+                        <th scope="col">Is Blocked</th>
+                        <th scope="col">Blocked By</th>
+                        <th scope="col">Created At</th>
+                        <th scope="col">Updated At</th>
+                        <th scope="col">Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($users as $user)
+                        <tr>
+                            <td>{{ $user->name }}</td>
+                            <td>{{ $user->email }}</td>
+                            <td>{{ $user->usertype }}</td>
+                            <td>{{ $user->is_blocked ? 'Yes' : 'No' }}</td>
+                            <td>{{ $user->blocked_by ?? 'N/A' }}</td>
+                            <td>{{ $user->created_at->format('Y-m-d H:i') }}</td>
+                            <td>{{ $user->updated_at->format('Y-m-d H:i') }}</td>
+                            <td>
+                                <a href="{{ route('admin.users.show', $user->id) }}"
+                                    class="btn btn-primary btn-sm">View</a>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
 </x-app-layout>
