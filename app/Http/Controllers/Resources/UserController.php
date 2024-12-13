c5e34616eb3bffa1364f3024a9fc115b423adaab
ianlabicani <labicaniian@gmail.com>
2024-12-13 22:45:01 +0800

    refactor UserController by removing unused methods and deleting the user edit view

--- app/Http/Controllers/Resources/UserController.php
+++ app/Http/Controllers/Resources/UserController.php
@@ -17,26 +17,7 @@ class UserController extends Controller
         $users = User::all();
         return view('admin.users.index', compact('users'));
     }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
+    
     public function show(string $id)
     {
         $user = User::find($id);
@@ -47,50 +28,4 @@ class UserController extends Controller
 
         return view('admin.users.show', ['user' => $user]);
     }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-
-        $user = User::find($id);
-
-        if (!$user) {
-            return redirect()->route('users.index')->with('error', 'User not found');
-        }
-
-        return view('admin.users.edit', ['user' => $user]);
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
-    }
-
-    public function updateUsertype(Request $request, $id)
-    {
-        $request->validate([
-            'usertype' => 'required|in:admin,user',
-        ]);
-
-        $user = User::findOrFail($id);
-        $user->usertype = $request->usertype;
-        $user->save();
-
-        return redirect()->route('users.index')->with('success', 'User type updated successfully!');
-    }
-
 }
