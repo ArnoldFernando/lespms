15417b365736ddb6a2bbc2c708827cb487572d9f
ianlabicani <labicaniian@gmail.com>
2024-12-13 22:44:45 +0800

    refactor BookingController and ServiceController by removing unused methods and improving code clarity

--- app/Http/Controllers/Resources/BookingController.php
+++ app/Http/Controllers/Resources/BookingController.php
@@ -20,14 +20,7 @@ class BookingController extends Controller
         $bookings = Booking::with('eventService')->where('user_id', auth()->id())->get();
         return view('Client.booking.index', compact('bookings'));
     }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
+    
 
     /**
      * Store a newly created resource in storage.
@@ -84,38 +77,12 @@ class BookingController extends Controller
         return redirect()->back()->with('success', 'Service booked successfully!');
     }
 
-
-
-
-    /**
-     * Display the specified resource.
-     */
     public function show(string $id)
     {
-        //
-    }
+        $booking = Booking::with('eventService')->findOrFail($id);
+        $eventService = $booking->eventService;
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
+        return view('Client.booking.show', compact('eventService'));
     }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
     }
 }
