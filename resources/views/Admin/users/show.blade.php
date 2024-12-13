9dafdeb9fb035e49383ada0e14178c11e096123a
ianlabicani <labicaniian@gmail.com>
2024-12-13 22:45:52 +0800

    update user show view to remove edit link and adjust back navigation route

--- resources/views/Admin/users/show.blade.php
+++ resources/views/Admin/users/show.blade.php
@@ -1,5 +1,4 @@
 <x-app-layout>
     <pre>{{ json_encode($user, JSON_PRETTY_PRINT) }}</pre>
-    <a href="{{ route('users.edit', $user->id) }}">Edit</a>
-    <a href="{{ route('users.index') }}">Back</a>
+    <a href="{{ route('admin.users.index') }}">Back</a>
 </x-app-layout>
