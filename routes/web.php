a98ae24adff6650a93a89e3cd447a11ffdf5500b
ianlabicani <labicaniian@gmail.com>
2024-12-13 22:45:33 +0800

    refactor routes by organizing service provider and client routes, improving clarity and structure

--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,10 @@ use App\Http\Controllers\Resources\ServiceController;
 use App\Http\Controllers\Resources\EventServiceController;
 use App\Http\Controllers\Resources\BookingController;
 use App\Http\Controllers\Resources\NotificationController;
-use App\Http\Controllers\Resources\BlockuserController;
+use App\Http\Controllers\ServiceProvider\BlockuserController;
 use App\Http\Controllers\ServiceProvider\DashboardController as ServiceProviderDashboardController;
+use App\Http\Controllers\Client\DashboardController as ClientDashboardController;
+
 use App\Mail\notifmail;
 use App\Livewire\Chat;
 
@@ -28,81 +30,63 @@ use App\Livewire\Chat;
 */
 
 Route::get('/', function () {
+    // TODO: make welcome page content have the data from the database
     return view('welcome');
 });
 
 Auth::routes();
 
-Route::get('/home', [HomeController::class, 'redirectUserBasedOnRole'])
-    ->middleware('auth')
-    ->name('home');
+Route::get('/home', [HomeController::class, 'redirectUserBasedOnRole'])->middleware('auth')->name('home');
 
-
-Route::middleware('auth')->group(function () {
-    // client
-    Route::get('/client/profile', [ProfileController::class, 'userprofile'])->name('user.profile');
-    Route::get('/clientprofile/edit', [ProfileController::class, 'editProfile'])->name('user.edit');
-    // service provider
-    Route::get('/service-provider/profile', [ProfileController::class, 'serviceprofile'])->name('service.profile');
-    Route::get('/service-provider/profile/edit', [ProfileController::class, 'editserviceProfile'])->name('service.profile.edit');
-
-    Route::put('profile/update', [ProfileController::class, 'updateProfile'])->name('user.update');
-});
+// admin routes
 
 Route::prefix('admin')
+    ->name('admin.')
     ->middleware(['auth', 'admin'])
     ->group(function () {
         Route::get('dashboard', [AdminDashboardController::class, 'getData'])
-            ->name('admin.dashboard');
-        Route::put('users/{id}/usertype', [UserController::class, 'updateUsertype'])
-            ->name('users.updateUsertype');
+            ->name('dashboard');
         Route::resource('users', UserController::class);
     });
 
+// service provider routes
+
 Route::prefix('service-provider')
+    ->name('service-provider.')
     ->middleware(['auth', 'service.provider'])
     ->group(function () {
-        Route::get('dashboard', [ServiceProviderDashboardController::class, 'index'])
-            ->name('service-provider.dashboard');
+        Route::get('dashboard', [ServiceProviderDashboardController::class, 'index'])->name('dashboard');
         Route::resource('services', EventServiceController::class);
-        Route::get('notifications/recent', [NotificationController::class, 'getRecentNotifications'])
-            ->name('notifications.getNotifications');
+        Route::get('notifications/recent', [NotificationController::class, 'getRecentNotifications'])->name('notifications.getNotifications');
         Route::resource('notifications', NotificationController::class);
-        Route::get('/my-services/bookings', [EventServiceController::class, 'showBookings'])
-            ->name('event-services.bookings');
-        Route::post('/my-services/bookings/{bookingId}/{status}', [EventServiceController::class, 'updateStatus'])
-            ->name('event-services.updateStatus');
-        Route::get('/booked-users', [BlockuserController::class, 'index'])
-            ->name('booked.users');
-        Route::post('/block-user/{user}/service', [BlockuserController::class, 'blockUserFromServices'])
-            ->name('user.block.service');
-        Route::patch('/user/unblock/{userId}', [BlockuserController::class, 'unblockUser'])
-            ->name('service.user.unblock');
+        Route::get('/my-services/bookings', [EventServiceController::class, 'showBookings'])->name('event-services.bookings');
+        Route::post('/my-services/bookings/{bookingId}/{status}', [EventServiceController::class, 'updateStatus'])->name('event-services.updateStatus');
+        Route::get('/booked-users', [BlockuserController::class, 'index'])->name('booked.users');
+        Route::post('/block-user/{user}/service', [BlockuserController::class, 'blockUserFromServices'])->name('user.block.service');
+        Route::patch('/user/unblock/{userId}', [BlockuserController::class, 'unblockUser'])->name('user.unblock');
+        Route::get('profile', [ProfileController::class, 'getClientProfile'])->name('profile');
+        Route::get('profile/edit', [ProfileController::class, 'editClientProfile'])->name('profile.edit');
+        Route::put('profile/update', [ProfileController::class, 'updateProfile'])->name('update');
+        Route::post('/block-user/{user}', [BlockuserController::class, 'blockUser'])->name('user.block');
+        Route::post('/unblock-user/{user}', [BlockuserController::class, 'unblockUser'])->name('user.unblock');
+        Route::get('/chat/{receiverId}', Chat::class)->name('chat');
     });
 
+// client routes
 
-// TODO: refactor the routes below
-// user routes
-Route::middleware(['auth', 'user'])->group(function () {
-    route::view('user', 'Client.dashboard');
-    Route::prefix('client')->name('client.')->group(function () {
-        route::resource('service', ServiceController::class);
-        route::get('servicedetails/{id}', [ServiceController::class, 'view_details'])
-            ->name('view-details');
-        route::resource('bookings', BookingController::class);
+Route::prefix('client')
+    ->name('client.')
+    ->middleware(['auth', 'client'])
+    ->group(function () {
+        Route::get('dashboard', [ClientDashboardController::class, 'index'])->name('dashboard');
+        Route::resource('service', ServiceController::class);
+        Route::resource('bookings', BookingController::class);
         Route::get('notifications/{id}', [NotificationController::class, 'showuser'])->name('notifications.show');
+        Route::get('profile', [ProfileController::class, 'getClientProfile'])->name('profile');
+        Route::get('profile/edit', [ProfileController::class, 'editClientProfile'])->name('edit');
+        Route::put('profile/update', [ProfileController::class, 'updateProfile'])->name('update');
+        Route::get('/chat/{receiverId}', Chat::class)->name('chat');
     });
-});
-
-
-Route::get('/service-provider/notifications', [NotificationController::class, 'getNotifications'])
-    ->name('service-provider.notifications');
-
-Route::middleware('auth')->group(function () {
-    Route::post('/block-user/{user}', [BlockuserController::class, 'blockUser'])->name('user.block');
-    Route::post('/unblock-user/{user}', [BlockuserController::class, 'unblockUser'])->name('user.unblock');
-});
-Route::get('/chat/{receiverId}', Chat::class)->name('chat');
 
 
 // Route::get('/testmail', function () {
