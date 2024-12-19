31fd84ec7ac7ca0876b2e448ae75e571a08bc53c
ianlabicani <labicaniian@gmail.com>
2024-12-19 12:29:03 +0800

    update admin users view to display verification status and remove timestamps

--- resources/views/Admin/users/index.blade.php
+++ resources/views/Admin/users/index.blade.php
@@ -9,21 +9,23 @@
                         <th scope="col">User Type</th>
                         <th scope="col">Is Blocked</th>
                         <th scope="col">Blocked By</th>
-                        <th scope="col">Created At</th>
-                        <th scope="col">Updated At</th>
+                        <th scope="col">Verified</th>
                         <th scope="col">Actions</th>
                     </tr>
                 </thead>
                 <tbody>
                     @foreach ($users as $user)
+                        @if ($user->id == Auth::id())
+                            @continue
+                        @endif
                         <tr>
                             <td>{{ $user->name }}</td>
                             <td>{{ $user->email }}</td>
                             <td>{{ $user->usertype }}</td>
                             <td>{{ $user->is_blocked ? 'Yes' : 'No' }}</td>
                             <td>{{ $user->blocked_by ?? 'N/A' }}</td>
-                            <td>{{ $user->created_at->format('Y-m-d H:i') }}</td>
-                            <td>{{ $user->updated_at->format('Y-m-d H:i') }}</td>
+                            <td class="{{ $user->verified ? 'text-success' : 'text-danger' }}">
+                                {{ $user->verified ? 'Yes' : 'No' }}</td>
                             <td>
                                 <a href="{{ route('admin.users.show', $user->id) }}"
                                     class="btn btn-primary btn-sm">View</a>
