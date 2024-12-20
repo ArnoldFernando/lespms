e4ca3c1db4a8403d6f0976d610f8d1812e9ebeaa
ianlabicani <labicaniian@gmail.com>
2024-12-21 02:29:15 +0800

    move the booking related functions to boooking controller

--- app/Http/Controllers/Resources/BookingController.php
+++ app/Http/Controllers/Resources/BookingController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Resources;
 use App\Http\Controllers\Controller;
 use App\Mail\notifmail;
 use App\Models\Booking;
+use App\Models\EventService;
+use App\Models\Notification;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -12,13 +14,85 @@ use Illuminate\Support\Facades\Mail;
 
 class BookingController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
+    public function index(Request $request)
     {
-        $bookings = Booking::with('eventService')->where('user_id', auth()->id())->get();
-        return view('Client.booking.index', compact('bookings'));
+        $query = Booking::with(['eventService' => function ($query) {
+                $query->select('id', 'service_provider_id');
+            }, 'user' => function ($query) {
+                $query->select('id', 'is_blocked');
+            }]);
+
+        $allowedStatuses = ['pending', 'confirmed', 'canceled', 'completed'];  // Define the allowed statuses
+
+         // Check if the request has a valid status and filter based on the allowed statuses
+        if ($request->has('status') && in_array($request->input('status'), $allowedStatuses)) {
+            $query->where('status', $request->input('status'));
+        } else {
+            // Redirect back with an error message if the status is invalid
+            return redirect()->route('service-provider.bookings.index', ['status' => 'pending'])
+                            ->with('error', 'Invalid status. Allowed statuses are: pending, confirmed, or canceled.');
+        }
+
+        $query->orderBy('booking_date');
+        $bookings = $query->get();
+
+        if (auth()->user()->usertype === "service_provider") {
+            $nonBlockedBookings = $bookings->where('user.is_blocked', false);
+            $blockedBookings = $bookings->where('user.is_blocked', true);
+
+            return view('Service.booked-service.index', compact('nonBlockedBookings', 'blockedBookings'));
+        }
+
+        if (auth()->user()->usertype === "user") {
+            return view('Client.booking.index', compact('bookings'));
+        }
+
+        return redirect()->route('home');
+    }
+
+
+    public function update(Request $request, $id)
+    {
+        $booking = Booking::find($id);
+        $status = $request->status;
+
+        $validStatuses = ['pending', 'confirmed', 'canceled', 'completed'];
+        
+        if (!in_array($status, $validStatuses)) {
+            return redirect()->back()->with('error', 'Invalid status');
+        }
+
+
+        $booking->status = $status;
+        $booking->save();
+
+        if ($status == 'confirmed') {
+            $eventService = EventService::find($booking->event_service_id);
+
+            if ($eventService) {
+                $eventService->status = 'unavailable';
+                $eventService->save();
+            }
+        }
+
+        if ($status == 'completed') {
+            $eventService = EventService::find($booking->event_service_id);
+
+            if ($eventService) {
+                $eventService->status = 'available';
+                $eventService->save();
+            }
+        }
+
+        Notification::create([
+            'user_id' => $booking->user_id,
+            'message' => 'Your booking has been ' . $status,
+            'booked_by_user_id' => auth()->id(),
+            'read' => false,
+        ]);
+
+        return redirect()->back()->with('success', 'Booking status updated successfully, and the user has been notified.');
+
     }
 
 
@@ -84,4 +158,5 @@ class BookingController extends Controller
 
         return view('Client.booking.show', compact('eventService'));
     }
+
 }
